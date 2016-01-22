aa597a4a79a63f97c9909169bb8c1b543fbc9859
bjarkithordarson <bjarki@thordarson.is>
2016-01-22 00:40:13 +0000

    Leiðréttingar

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 console.log("Loading hut-servicedesk-app-info-plugin...");
 
+var fs = require('fs');
+
 function Plugin(context) {
-	console.log(context);
+
+	context.UI.addTab("hut-servicedesk-app-info-plugin", "Upplýsingar", "info-circle", function() {
+
+		fs.readFile(__dirname + '/index.html', 'utf8', function(error, data) {
+			context.UI.loadContent(data);
+			context.UI.selectTab("hut-servicedesk-app-info-plugin");
+		});
+
+	});
+
 }
 
 module.exports = Plugin
